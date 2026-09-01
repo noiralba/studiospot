0b44791b1222a9a00e43ee13717e53bfbe8402a5
sarakemppi <kemppisara@gmail.com>
2026-09-01 21:29:17 +0200

    add manage booking page

---
+++ src/pages/ManageBooking.tsx
@@ -0,0 +1,9 @@
+export default function ManageBooking() {
+  return (
+    <main>
+      <h1>Hantera bokning</h1>
+
+      <p>Här kan du hitta, ändra eller avboka din bokning.</p>
+    </main>
+  );
+}
