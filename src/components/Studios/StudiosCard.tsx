6a1ed4103f27399f57ea2d1c54b232b32db27748
Andrea V <andreavega89.av@gmail.com>
2026-09-01 17:35:33 +0200

    added all studios component and studios page

--- src/components/Studios/StudiosCard.tsx
+++ src/components/Studios/StudiosCard.tsx
@@ -8,10 +8,10 @@ interface StudioCardProps {
   description: string;
   imageUrl: string;
   pricePerHour: number;
-
 }
 
-export default function StudiosCard({ name, description, imageUrl, pricePerHour }: StudioCardProps) {
+//hämtar viss del av datan från db.json och visar den i en card komponent.
+export default function StudiosCard({ name, description, imageUrl, pricePerHour}: StudioCardProps) {
   return (
     <section className={styles.studioCard}>
       <img src={imageUrl} alt={`${name} studio`} className={styles.image} />
@@ -19,7 +19,7 @@ export default function StudiosCard({ name, description, imageUrl, pricePerHour 
       <p className={styles.description}>{description}</p>
       <p className={styles.price}>Price per hour: {pricePerHour} SEK</p>
       {/* <Link to={`studios/${studioId}`}> */}
-      <Button type="button" children="Läs mer" />
+      <Button type="button" children="View Details" />
     {/* </Link> */}
     </section>
   );
