2e1527bd52f4fa4db0a49f2a5e1df3feaf06bf85
Nehac210401 <chatrathneha2@gmail.com>
2026-09-02 02:31:02 +0200

    Created bookingValidation where the function will check whether there is overlap or not, also another will check that end time is always after start time and a function for cross checking of double booking. A booking.ts (utils file) for exporting the interface of both studio and booking to be confirmed during the validaion.

---
+++ src/components/types/Booking.ts
@@ -0,0 +1,21 @@
+export interface Studio {
+  id: number;
+  name: string;
+  capacity: number;
+  pricePerHour: number;
+  category: "photo" | "podcast" | "music" | "video";
+  imageUrl: string;
+  description: string;
+  equipment?: string[];
+}
+
+export interface Booking {
+  id: number;
+  studioId: number;
+  email: string;
+  startTime: string;
+  endTime: string;
+  status: "confirmed" | "cancelled";
+}
+
+export type NewBooking = Omit<Booking, "id">;
