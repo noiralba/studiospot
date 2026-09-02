ec49339e7fb3602fbe50220fd6c7922cb7fd2f66
tacokrydda <jbtand19@gmail.com>
2026-09-02 12:26:24 +0200

    wip: save work

--- src/pages/Booking.tsx
+++ src/pages/Booking.tsx
@@ -1,31 +1,75 @@
-import type { SubmitEvent } from "react";
-import { useParams } from "react-router";
+import { Form, useParams, redirect, useActionData } from "react-router";
+import type { ActionFunctionArgs } from "react-router";
 import "../styles/_Booking.scss";
 
 import FormGroup from "../components/FormGroup/FormGroup";
 import Button from "../components/Button/Button";
 
+export async function action({ request, params }: ActionFunctionArgs) {
+  const formData = await request.formData();
+
+  const bookingData = {
+    studioId: params.studioId,
+    date: formData.get("date") as string,
+    startTime: formData.get("startTime") as string,
+    endTime: formData.get("endTime") as string,
+    email: formData.get("email") as string,
+  };
+
+  // middleware
+
+  try {
+    const response = await fetch("http://localhost:3000/bookings", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(bookingData),
+    });
+
+    if (!response.ok) {
+      return { error: "Kunde inte spara bokningen på servern." };
+    }
+
+    return redirect("/booking-success");
+  } catch (error) {
+    return { error: "Nätverksfel. Försök igen senare." };
+  }
+}
+
 export default function Booking() {
   const { studioId } = useParams<{ studioId: string }>();
-
-  const handleSubmit = (e: SubmitEvent<HTMLFormElement>) => {
-    e.preventDefault();
-  };
+  const actionData = useActionData() as { error?: string } | undefined;
 
   return (
     <main className="booking-page">
       <h1>Boka studio</h1>
       <p>Du bokar studio med ID: {studioId}</p>
 
-      <form onSubmit={handleSubmit}>
-        <FormGroup id="date" label="Datum" type="date" required />
+      {actionData?.error && <p className="error-message">{actionData.error}</p>}
 
-        <FormGroup id="startTime" label="Starttid" type="time" required />
+      <Form method="post">
+        <FormGroup id="date" name="date" label="Datum" type="date" required />
 
-        <FormGroup id="endTime" label="Sluttid" type="time" required />
+        <FormGroup
+          id="startTime"
+          name="startTime"
+          label="Starttid"
+          type="time"
+          required
+        />
+
+        <FormGroup
+          id="endTime"
+          name="endTime"
+          label="Sluttid"
+          type="time"
+          required
+        />
 
         <FormGroup
           id="email"
+          name="email"
           label="E-post"
           type="email"
           placeholder="din@email.com"
@@ -33,7 +77,7 @@ export default function Booking() {
         />
 
         <Button type="submit" children="Boka studio" />
-      </form>
+      </Form>
     </main>
   );
 }
